ee5d53a2109e1f4d3b2bbc204e6726299ec77345
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-17 02:45:26 +0400

    add types

---
+++ types.d.ts
@@ -0,0 +1,25 @@
+type FeatureCardProps = {
+  title: string;
+  desc: string;
+  iconName: any;
+};
+
+type ProjectCardProps = {
+  title: string;
+  desc: string;
+  imgSrc: any;
+  imgAlt: string;
+  projUrl: string;
+};
+
+type ExperienceProp = {
+  title: string;
+  time: string;
+  desc: string;
+};
+
+type EducationProp = {
+  title: string;
+  time: string;
+  desc: string;
+};
