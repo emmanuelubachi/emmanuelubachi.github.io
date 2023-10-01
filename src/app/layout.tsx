b0ce0d03112371b3098799f7ac3e34986762b88d
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-02 02:59:00 +0400

    update page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Footer from "@/components/body/Footer";
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -17,10 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
