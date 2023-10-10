03a38379bb9666b6bf92136ddef95e88cc13ec69
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-10 04:24:59 +0400

    update pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Features from "@/components/sections/Features";
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
 import Footer from "@/components/body/Footer";
+import Resume from "@/components/sections/Resume";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       <main>
         <Features />
         <Projects />
-        {/* <Resume /> */}
+        <Resume />
         {/* <Contact /> */}
         <Footer />
       </main>
