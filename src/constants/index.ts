d5864d406442141c92f1f6175b06d102b7903c9e
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-19 13:12:53 +0400

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,160 @@
+export const PROJECT = [
+  {
+    title: "Hilink Travel App - Putuk Truno Camp Area",
+    desc: "Designed and developed a travel app for exploring the Putuk Truno Camp Area. Implemented UI/UX enhancements for a user-friendly experience. Utilized React.js and Next.js for seamless navigation.",
+    imgSrc: "/travelApp.png",
+    imgAlt: "Hilink Travel App - Putuk Truno Camp Area Project Image",
+    projUrl: "https://travel-app-pi-beryl.vercel.app/",
+    badge: [
+      { label: "UI/UX" },
+      { label: "React.js" },
+      { label: "Next.js" },
+      { label: "Tailwind CSS" },
+    ],
+    inverse: true,
+    isLink: true,
+  },
+  {
+    title: "Car Hub - Find, Book, or rent a car — quicly and easily!",
+    desc: "Developed an online car hub platform for finding, booking, and renting cars with ease. Leveraged React, Next.js, and TypeScript to ensure a seamless and efficient user experience while optimizing for search engine visibility (SEO).",
+    imgSrc: "/carhub.png",
+    imgAlt: "Car Hub - Find, Book, or Rent a Car Project Image",
+    projUrl: "https://car-showcase-roan-nu.vercel.app/",
+    badge: [
+      { label: "React" },
+      { label: "Next JS 13" },
+      { label: "TypeScript" },
+      { label: "SEO" },
+    ],
+    inverse: false,
+    isLink: true,
+  },
+  {
+    title:
+      "Pacific Food Trade: Data-Driven Insights and Data Visualization Development",
+    desc: "Developed on the Pacific Food Trade project to deliver data-driven insights through data visualization and Python analysis. Utilized JavaScript and Next.js for frontend development, incorporating Echarts for compelling visualizations.",
+    imgSrc: "/pacificfood.png",
+    imgAlt: "Pacific Food Trade Project Image",
+    projUrl: "https://ubachi.com/feeding-the-pacific/",
+    badge: [
+      { label: "JavaScript" },
+      { label: "Python" },
+      { label: "Echarts" },
+      { label: "Next.js" },
+    ],
+    inverse: true,
+    isLink: true,
+  },
+  {
+    title: "Trade Flow Dashboard of Copper & Cobalt Production in the DR Congo",
+    desc: "Built Power BI dashboards to visualize Copper & Cobalt production in DR Congo by analyzing data with Python. Created models and visuals that provided insights for data-driven decisions on mining complexities.",
+    imgSrc: "/tradeflow.png",
+    imgAlt:
+      "Trade Flow Dashboard of Copper & Cobalt Production in the DR Congo Project Image",
+    projUrl: "https://chinaglobalsouth.com/cobaltmap/en/production-flow.html",
+    badge: [
+      { label: "Power BI" },
+      { label: "Python" },
+      { label: "Figma" },
+      { label: "UI/UX" },
+    ],
+    inverse: false,
+    isLink: false,
+  },
+  {
+    title: "Interactive Map of Copper & Cobalt Production in the DR Congo",
+    desc: "Created interactive map of Copper & Cobalt production in DR Congo using visualizations and data from mining registry and field research. Delivered visually compelling insights to industry stakeholders.",
+    imgSrc: "/interactivemap.png",
+    imgAlt:
+      "Interactive Map of Copper & Cobalt Production in the DR Congo Project Image",
+    projUrl: "#",
+    badge: [
+      { label: "Figma" },
+      { label: "UI/UX" },
+      { label: "HTML" },
+      { label: "CSS" },
+      { label: "JavaScript" },
+      { label: "JavaScript" },
+    ],
+    inverse: true,
+    isLink: false,
+  },
+];
+
+export const ICONS = [
+  {
+    src: "/icons/azure.png",
+    alt: "azure logo",
+  },
+  {
+    src: "/icons/excel.png",
+    alt: "Excel logo",
+  },
+  {
+    src: "/icons/powerbi.png",
+    alt: "Power BI logo",
+  },
+  {
+    src: "/icons/python.png",
+    alt: "Python logo",
+  },
+  {
+    src: "/icons/r.png",
+    alt: "R logo",
+  },
+  {
+    src: "/icons/tableau.png",
+    alt: "Tableau logo",
+  },
+  // ----------------------------------------
+  {
+    src: "/icons/azure.png",
+    alt: "azure logo",
+  },
+  {
+    src: "/icons/excel.png",
+    alt: "Excel logo",
+  },
+  {
+    src: "/icons/powerbi.png",
+    alt: "Power BI logo",
+  },
+  {
+    src: "/icons/python.png",
+    alt: "Python logo",
+  },
+  {
+    src: "/icons/r.png",
+    alt: "R logo",
+  },
+  {
+    src: "/icons/tableau.png",
+    alt: "Tableau logo",
+  },
+  // -----------------------------------------
+  {
+    src: "/icons/azure.png",
+    alt: "azure logo",
+  },
+  {
+    src: "/icons/excel.png",
+    alt: "Excel logo",
+  },
+  {
+    src: "/icons/powerbi.png",
+    alt: "Power BI logo",
+  },
+  {
+    src: "/icons/python.png",
+    alt: "Python logo",
+  },
+  {
+    src: "/icons/r.png",
+    alt: "R logo",
+  },
+  {
+    src: "/icons/tableau.png",
+    alt: "Tableau logo",
+  },
+  // --------------------------------------------
+];
