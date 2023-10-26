a9e5b5ff97bd007a74d58327cdab7f894736543c
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-26 13:29:00 +0400

    update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,51 +1,5 @@
 export const PROJECT = [
   {
-    title: "Hilink Travel App - Putuk Truno Camp Area",
-    desc: "Developed a travel app for exploring the Putuk Truno Camp Area. Implemented UI/UX enhancements for a user-friendly experience. Utilized React.js and Next.js for seamless navigation.",
-    imgSrc: "/travelApp.png",
-    imgAlt: "Hilink Travel App - Putuk Truno Camp Area Project Image",
-    projUrl: "https://travel-app-pi-beryl.vercel.app/",
-    badge: [
-      { label: "UI/UX" },
-      { label: "React.js" },
-      { label: "Next.js" },
-      { label: "Tailwind CSS" },
-    ],
-    inverse: true,
-    isLink: true,
-  },
-  {
-    title: "Car Hub - Find, Book, or rent a car — quicly and easily!",
-    desc: "Developed an online car hub platform for finding, booking, and renting cars with ease. Leveraged React, Next.js, and TypeScript to ensure a seamless and efficient user experience while optimizing for search engine visibility (SEO).",
-    imgSrc: "/carhub.png",
-    imgAlt: "Car Hub - Find, Book, or Rent a Car Project Image",
-    projUrl: "https://car-showcase-roan-nu.vercel.app/",
-    badge: [
-      { label: "React" },
-      { label: "Next JS 13" },
-      { label: "TypeScript" },
-      { label: "SEO" },
-    ],
-    inverse: false,
-    isLink: true,
-  },
-  {
-    title:
-      "Pacific Food Trade: Data-Driven Insights and Data Visualization Development",
-    desc: "Designed and developed on the Pacific Food Trade project to deliver data-driven insights through data visualization and Python analysis. Utilized JavaScript and Next.js for frontend development, incorporating Echarts for compelling visualizations.",
-    imgSrc: "/pacificfood.png",
-    imgAlt: "Pacific Food Trade Project Image",
-    projUrl: "https://ubachi.com/feeding-the-pacific/",
-    badge: [
-      { label: "JavaScript" },
-      { label: "Python" },
-      { label: "Echarts" },
-      { label: "Next.js" },
-    ],
-    inverse: true,
-    isLink: true,
-  },
-  {
     title: "Trade Flow Dashboard of Copper & Cobalt Production in the DR Congo",
     desc: "Designed and developed Power BI dashboards to visualize Copper & Cobalt production in DR Congo by analyzing data with Python. Created models and visuals that provided insights for data-driven decisions on mining complexities.",
     imgSrc: "/tradeflow.png",
@@ -59,7 +13,23 @@ export const PROJECT = [
       { label: "UI/UX" },
     ],
     inverse: false,
-    isLink: false,
+    isLink: true,
+  },
+  {
+    title:
+      "Pacific Food Trade: Data-Driven Insights and Data Visualization Development",
+    desc: "Designed and developed on the Pacific Food Trade project to deliver data-driven insights through data visualization and Python analysis. Utilized JavaScript and Next.js for frontend development, incorporating Echarts for compelling visualizations.",
+    imgSrc: "/pacificfood.png",
+    imgAlt: "Pacific Food Trade Project Image",
+    projUrl: "https://pacific-food-trade.ubachi.com",
+    badge: [
+      { label: "JavaScript" },
+      { label: "Python" },
+      { label: "Echarts" },
+      { label: "Next.js" },
+    ],
+    inverse: true,
+    isLink: true,
   },
   {
     title: "Interactive Map of Copper & Cobalt Production in the DR Congo",
@@ -67,7 +37,7 @@ export const PROJECT = [
     imgSrc: "/interactivemap.png",
     imgAlt:
       "Interactive Map of Copper & Cobalt Production in the DR Congo Project Image",
-    projUrl: "#",
+    projUrl: "https://chinaglobalsouth.com/cobaltmap/en/",
     badge: [
       { label: "Figma" },
       { label: "UI/UX" },
@@ -76,8 +46,39 @@ export const PROJECT = [
       { label: "JavaScript" },
       { label: "JavaScript" },
     ],
+    inverse: false,
+    isLink: true,
+  },
+  {
+    title: "Car Hub - Find, Book, or rent a car — quicly and easily!",
+    desc: "Developed an online car hub platform for finding, booking, and renting cars with ease. Leveraged React, Next.js, and TypeScript to ensure a seamless and efficient user experience while optimizing for search engine visibility (SEO).",
+    imgSrc: "/carhub.png",
+    imgAlt: "Car Hub - Find, Book, or Rent a Car Project Image",
+    projUrl: "https://carhub.ubachi.com",
+    badge: [
+      { label: "React" },
+      { label: "Next JS 13" },
+      { label: "TypeScript" },
+      { label: "SEO" },
+    ],
     inverse: true,
-    isLink: false,
+    isLink: true,
+  },
+
+  {
+    title: "Hilink Travel App - Putuk Truno Camp Area",
+    desc: "Developed a travel app for exploring the Putuk Truno Camp Area. Implemented UI/UX enhancements for a user-friendly experience. Utilized React.js and Next.js for seamless navigation.",
+    imgSrc: "/travelApp.png",
+    imgAlt: "Hilink Travel App - Putuk Truno Camp Area Project Image",
+    projUrl: "https://hilink.ubachi.com",
+    badge: [
+      { label: "UI/UX" },
+      { label: "React.js" },
+      { label: "Next.js" },
+      { label: "Tailwind CSS" },
+    ],
+    inverse: false,
+    isLink: true,
   },
 ];
 
